84855f500fd44c69f1e6562df9775a51e4adb674
klevan66 <54721972+klevan66@users.noreply.github.com>
2024-11-19 11:00:49 +0100

    0.0.2 updated db

--- LoginWindow.py
+++ LoginWindow.py
@@ -1,6 +1,13 @@
 from tkinter import *
 from PIL import ImageTk, Image
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "1234",
+    database="portfoliodb"
+)
 class LoginForm:
     def __init__(self, window):
         self.window = window
@@ -26,6 +33,15 @@ class LoginForm:
         self.txt= "COIN MANAGER"
         self.heading = Label(self.loginFrame, text=self.txt, font=("arial",30,"bold"), bg="black", fg="white")
         self.heading.place(x=30, y=80, width=500, height=30)
+
+        # sign in image
+
+        # username
+        self.usernameLabel = Label(self.loginFrame, text="Username", bg="black", font=("arial", 13, "bold"), fg="black")
+        self.usernameLabel.place(x=550, y=300)
+        self.usernameEntry = Entry(self.loginFrame, highlightthickness=0)
+
+
 
 
 def page():
